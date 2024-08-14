cf4c8e402fceb8bdcab4a865c8a57a57e9c72229
bstnp <54482913+BastianPEIRE@users.noreply.github.com>
2024-08-14 07:51:49 +0000

    wip rcon + volume docker

--- control-panel/app/api/server/command/route.ts
+++ control-panel/app/api/server/command/route.ts
@@ -1,11 +1,32 @@
-export async function POST() {
-
-    const status = true;
-    sendCommand("minecraft-server");
-
-
+interface Request {
+    command: string;
 }
 
-function sendCommand(arg0: string) {
-    throw new Error("Function not implemented.");
+export async function POST(request: Request) {
+    try {
+        const data: Request = await request.json();
+        const command = data.command;
+
+        console.log(command);
+    
+        return NextResponse.json({ message: 'Command sended successfully', data });
+    } catch (error) {
+        return NextResponse.json({ message: 'Error processing request', error }, { status: 500 });
+    }
+    //const status = true;
+    //const rcon = await Rcon.connect({
+    //    host: "localhost", port: 25575, password: "1234"
+    //})
+    
+    //let responses = await Promise.all([
+    //    rcon.send("help"),
+    //    rcon.send("whitelist list")
+    //])
+    
+    //for (response of responses) {
+    //    console.log(response)
+    //}
+    
+    rcon.end()
 }
+
